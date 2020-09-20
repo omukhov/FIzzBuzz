3e175c8fe9a27f8a6ae95b364a6e6dbaad491063
a.n.omukhov@volsu.ru <a.n.omukhov@volsu.ru>
2020-09-20 20:53:37 +0400

    Добавил вторую функцию и изменил main

--- dotNetProject1/Program.cs
+++ dotNetProject1/Program.cs
@@ -6,6 +6,20 @@ namespace dotNetProject1
     {
         static void Main(string[] args)
         {
+            
+            Console.Write("Выберите функцию, если нужно узнать простые числа введите 1, если FizzBuzz введите 2: ");
+            int k = Convert.ToInt32(Console.ReadLine());
+            if (k == 1)
+            {
+                SimpleNumbers();
+            }
+            if (k == 2)
+            {
+                FizzBuzz();
+            }
+        }
+        static void SimpleNumbers() 
+        {
             Console.Write("Введите количество нужных вам простых чисел: ");
             int n = Convert.ToInt32(Console.ReadLine());
             for (int i = 2; i < n; i++)
@@ -21,9 +35,33 @@ namespace dotNetProject1
                 if (b)
                 {
                     Console.Write("{0} ", i);
-                }                
+                }
             }
             Console.ReadKey();
+        }
+        static void FizzBuzz()
+        {
+            Console.Write("Введите количество нужных вам чисел для функции: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            for (int i = 1; i < n; i++)
+            {
+                if (i % 3 == 0 && i % 5 == 0)
+                {
+                    Console.WriteLine("FizzBuzz");
+                }
+                else if (i % 3 == 0)
+                {
+                    Console.WriteLine("Fizz");
+                }
+                else if (i % 5 == 0)
+                {
+                    Console.WriteLine("Buzz");
+                }
+                else
+                {
+                    Console.WriteLine(i);
+                }
+            }
         }
     }
 }
